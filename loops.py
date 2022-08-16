b720e9aafa46dc30bbc485fa1b03e2c0649c937e
avantiikasaxena15 <82288627+avantiikasaxena15@users.noreply.github.com>
2022-08-17 00:32:37 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,11 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+print("Positive numbers in the list 1: ")
+for pos_nums in list1:
+   if pos_nums > 0:
+      print(pos_nums, end = " ")
+print("\nPositive numbers in the list 2: ")
+for pos_nums in list2:
+   if pos_nums > 0:
+      print(pos_nums, end = " ")
+
